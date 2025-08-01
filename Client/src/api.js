396a98a99e4381ea2022187e8446b7aa2f5e99c5
ananda krishnan <202474683+ak-repo@users.noreply.github.com>
2025-08-01 14:13:46 +0530

    added Render api to project

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,4 +1,4 @@
-export const usersAPI = " http://localhost:8000/users"
-export const productsAPI = "http://localhost:3000/products";
+export const usersAPI = "http://0.0.0.0:10000/users";
+export const productsAPI = "http://0.0.0.0:10000/products";
 
 export const GST = 5;
